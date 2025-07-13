4290cf2521e01f6aa8887708dd818a2ab76202dc
Andre Postiga <andre.postiga@msn.com>
2025-07-13 18:57:54 -0300

    remove files

--- assets/excluir/fases/cozinha/tileset-cozinha.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="tileset-cozinha" tilewidth="64" tileheight="64" spacing="1" tilecount="28" columns="7">
- <image source="sprite-cozinha.png" width="454" height="259"/>
-</tileset>
